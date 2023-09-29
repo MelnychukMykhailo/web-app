4534951b8911863ae2d5e4580545ece6f172dff9
Melnychuk <melnychukmyhailo@gmail.com>
2023-09-29 15:20:38 +0300

    Project created.

---
+++ src/main/java/melnychuck/webapp/WebAppApplication.java
@@ -0,0 +1,13 @@
+package melnychuck.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppApplication.class, args);
+    }
+
+}
