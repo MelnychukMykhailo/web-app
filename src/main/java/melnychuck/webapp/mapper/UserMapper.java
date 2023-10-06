aeec06fd340a0e13e792729949e78553734ea959
Melnychuk <melnychukmyhailo@gmail.com>
2023-10-06 13:56:30 +0300

    UserMapper was created.

---
+++ src/main/java/melnychuck/webapp/mapper/UserMapper.java
@@ -0,0 +1,34 @@
+package melnychuck.webapp.mapper;
+
+import melnychuck.webapp.dto.request.UserRequestDto;
+import melnychuck.webapp.dto.response.UserResponseDto;
+import melnychuck.webapp.model.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    public UserResponseDto mapToDto(User user) {
+        UserResponseDto responseDto = new UserResponseDto();
+        responseDto.setId(user.getId());
+        responseDto.setEmail(user.getEmail());
+        responseDto.setFirstName(user.getFirstName());
+        responseDto.setLastName(user.getLastName());
+        responseDto.setBirthDate(user.getBirthDate());
+        responseDto.setAddress(user.getAddress());
+        responseDto.setPhoneNumber(user.getPhoneNumber());
+        return responseDto;
+    }
+
+    public User mapToModel(UserRequestDto requestDto) {
+        User user = new User();
+        user.setEmail(requestDto.getEmail());
+        user.setFirstName(requestDto.getFirstName());
+        user.setLastName(requestDto.getLastName());
+        user.setBirthDate(requestDto.getBirthDate());
+        user.setAddress(requestDto.getAddress());
+        user.setPhoneNumber(requestDto.getPhoneNumber());
+        return user;
+    }
+
+}
